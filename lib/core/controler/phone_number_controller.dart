0544109e2f0ed77dfa8fbbe4065955223a3bd3c5
Mo7amed <nofalseo306@gmail.com>
2024-12-18 12:43:33 +0200

    phone nmber and bundle id changed

--- lib/core/controler/phone_number_controller.dart
+++ lib/core/controler/phone_number_controller.dart
@@ -1,12 +1,11 @@
-import 'package:get/get.dart';
-import 'package:intl_phone_number_input/intl_phone_number_input.dart';
+// import 'package:get/get.dart';
 
-class PhoneNumberController extends GetxController {
-  var phoneNumber = ''.obs; // رقم الهاتف
-  var countryCode = ''.obs; // كود الدولة
+// class PhoneNumberController extends GetxController {
+//   var phoneNumber = ''.obs; // رقم الهاتف
+//   var countryCode = ''.obs; // كود الدولة
 
-  void updatePhoneNumber(PhoneNumber number) {
-    phoneNumber.value = number.phoneNumber ?? '';
-    countryCode.value = number.isoCode ?? '';
-  }
-}
+//   void updatePhoneNumber(PhoneNumber number) {
+//     phoneNumber.value = number.phoneNumber ?? '';
+//     countryCode.value = number.isoCode ?? '';
+//   }
+// }
--- lib/core/widget/phone_namber.dart
+++ lib/core/widget/phone_namber.dart
@@ -1,51 +1,50 @@
-import 'package:flutter/material.dart';
-import 'package:horsely_app/core/controler/Phone_number_controller.dart';
-import 'package:intl_phone_number_input/intl_phone_number_input.dart';
-import 'package:get/get.dart';
+// import 'package:flutter/material.dart';
+// import 'package:horsely_app/core/controler/Phone_number_controller.dart';
+// import 'package:get/get.dart';
 
-class PhoneNumberField extends StatelessWidget {
-  const PhoneNumberField({super.key});
+// class PhoneNumberField extends StatelessWidget {
+//   const PhoneNumberField({super.key});
 
-  @override
-  Widget build(BuildContext context) {
-    final PhoneNumberController controller = Get.put(PhoneNumberController());
-    return Padding(
-      padding: const EdgeInsets.all(16.0),
-      child: Column(
-        children: [
-          InternationalPhoneNumberInput(
-            onInputChanged: (PhoneNumber number) {
-              controller
-                  .updatePhoneNumber(number); // تحديث عند تغيير رقم الهاتف
-            },
-            selectorConfig: const SelectorConfig(
-              selectorType:
-                  PhoneInputSelectorType.DROPDOWN, // نوع اختيار الدولة
-            ),
-            ignoreBlank: false,
-            autoValidateMode: AutovalidateMode.onUserInteraction,
+//   @override
+//   Widget build(BuildContext context) {
+//     final PhoneNumberController controller = Get.put(PhoneNumberController());
+//     return Padding(
+//       padding: const EdgeInsets.all(16.0),
+//       child: Column(
+//         children: [
+//           InternationalPhoneNumberInput(
+//             onInputChanged: (PhoneNumber number) {
+//               controller
+//                   .updatePhoneNumber(number); // تحديث عند تغيير رقم الهاتف
+//             },
+//             selectorConfig: const SelectorConfig(
+//               selectorType:
+//                   PhoneInputSelectorType.DROPDOWN, // نوع اختيار الدولة
+//             ),
+//             ignoreBlank: false,
+//             autoValidateMode: AutovalidateMode.onUserInteraction,
             
-            initialValue: PhoneNumber(isoCode: 'EG'), // الدولة الافتراضية
-            inputDecoration: const InputDecoration(
-              labelText: 'Phone Number',
-              border: OutlineInputBorder(
-                borderSide: BorderSide(),
-              ),
-            ),
-          ),
-          const SizedBox(height: 16),
-          // Display the selected phone number and country code using GetX
-          Obx(() {
-            return Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text('Phone Number: ${controller.phoneNumber.value}'),
-                Text('Country Code: ${controller.countryCode.value}'),
-              ],
-            );
-          }),
-        ],
-      ),
-    );
-  }
-}
+//             initialValue: PhoneNumber(isoCode: 'EG'), // الدولة الافتراضية
+//             inputDecoration: const InputDecoration(
+//               labelText: 'Phone Number',
+//               border: OutlineInputBorder(
+//                 borderSide: BorderSide(),
+//               ),
+//             ),
+//           ),
+//           const SizedBox(height: 16),
+//           // Display the selected phone number and country code using GetX
+//           Obx(() {
+//             return Column(
+//               crossAxisAlignment: CrossAxisAlignment.start,
+//               children: [
+//                 Text('Phone Number: ${controller.phoneNumber.value}'),
+//                 Text('Country Code: ${controller.countryCode.value}'),
+//               ],
+//             );
+//           }),
+//         ],
+//       ),
+//     );
+//   }
+// }
