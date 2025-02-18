2175620bab4cfa4706493126fc1288824cac1396
Mo7amed <nofalseo306@gmail.com>
2025-02-18 16:03:29 +0200

    retry fixed

--- lib/core/widget/custom_retry_widget.dart
+++ lib/core/widget/custom_retry_widget.dart
@@ -15,37 +15,39 @@ class RetryWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      crossAxisAlignment: CrossAxisAlignment.center,
-      children: [
-        // SvgPicture.asset(AppImages.error),
-        const Icon(Icons.dangerous_rounded,
-            color: Color.fromARGB(255, 238, 72, 72), size: 100),
-        const SizedBox(
-          height: 15,
-          width: 1,
-        ),
-        ElevatedButton.icon(
-          onPressed: () async {
-            await onRetry();
-          },
-          label: Text(
-            AppStrings.tryAgain.tr,
-            style: const TextStyle(color: AppColors.white, fontSize: 18),
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          // SvgPicture.asset(AppImages.error),
+          const Icon(Icons.dangerous_rounded,
+              color: Color.fromARGB(255, 238, 72, 72), size: 100),
+          const SizedBox(
+            height: 15,
+            width: 1,
           ),
-          style: const ButtonStyle(
-            padding: WidgetStatePropertyAll(
-              EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+          ElevatedButton.icon(
+            onPressed: () async {
+              await onRetry();
+            },
+            label: Text(
+              AppStrings.tryAgain.tr,
+              style: const TextStyle(color: AppColors.white, fontSize: 18),
             ),
-            backgroundColor: WidgetStatePropertyAll(AppColors.primaryColor),
+            style: const ButtonStyle(
+              padding: WidgetStatePropertyAll(
+                EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+              ),
+              backgroundColor: WidgetStatePropertyAll(AppColors.primaryColor),
+            ),
+            icon: const Icon(
+              Icons.refresh,
+              color: AppColors.white,
+            ),
           ),
-          icon: const Icon(
-            Icons.refresh,
-            color: AppColors.white,
-          ),
-        ),
-      ],
+        ],
+      ),
     );
   }
 }
