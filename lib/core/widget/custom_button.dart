5a418cc9695e94fd309b1867727d0436a5bc8b11
ahmed-sabbagh-js <nofalseo304@gmail.com>
2025-02-27 10:48:17 +0200

    avvvv

--- lib/core/widget/custom_button.dart
+++ lib/core/widget/custom_button.dart
@@ -29,6 +29,7 @@ class CustomButton extends StatelessWidget {
       width: double.infinity,
       height: 56,
       child: TextButton(
+        
         style: TextButton.styleFrom(
           shape: RoundedRectangleBorder(
             borderRadius: BorderRadius.circular(15.r),
