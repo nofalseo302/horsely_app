c855f88f4a91f5307a0aa82fc80e13e562a8b244
ahmed-sabbagh-js <nofalseo304@gmail.com>
2025-03-02 14:35:14 +0200

    offer detailse data

--- lib/features/account/features/offer_details.dart/presentation/view/widget/info_offer.dart
+++ lib/features/account/features/offer_details.dart/presentation/view/widget/info_offer.dart
@@ -1,20 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:horsely_app/core/services/translation/app_string.dart';
-import 'package:horsely_app/core/widget/custom_button.dart';
+import 'package:horsely_app/core/utils/app_text_styles.dart';
 import 'package:horsely_app/features/account/features/offer/data/model/offers_model/datum.dart';
+import 'package:horsely_app/features/home/data/model/user_home_data/datum.dart';
 import 'package:horsely_app/features/home/presentation/view/widget/buy_widgets/iteam_buy_way.dart';
 import 'package:horsely_app/features/home/presentation/view/widget/titel_and_vule.dart';
-import 'package:horsely_app/features/account/features/offer_details.dart/presentation/view/screen/offer_details.dart';
 
-import '../../../../../../core/utils/app_text_styles.dart';
-
-class InformationOffer extends StatelessWidget {
-  const InformationOffer({
-    super.key,
-    required this.offer,
-  });
-  final OfferModel offer;
+class InfoOffer extends StatelessWidget {
+  const InfoOffer(
+      {super.key,
+      // required this.titel,
+      // required this.vule,
+      required this.p2pItem});
+  // final String titel, vule;
+  final OfferModel p2pItem;
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -40,8 +40,8 @@ class InformationOffer extends StatelessWidget {
               height: 16,
             ),
             TitelAndVule(
-              titel: AppStrings.price.tr,
-              vule: '${offer.currency?.symbol ?? ""} ${offer.price ?? ""}',
+              titel: AppStrings.amount.tr,
+              vule: " ${p2pItem.price ?? ""} ${p2pItem.currency?.symbol ?? ""}",
             ),
             const SizedBox(
               height: 12,
@@ -49,39 +49,28 @@ class InformationOffer extends StatelessWidget {
             TitelAndVule(
               titel: AppStrings.wants.tr,
               vule:
-                  '${offer.amount ?? ""} ${offer.cryptoCurrency?.symbol ?? ""}',
+                  " ${p2pItem.amount ?? ""} ${p2pItem.cryptoCurrency?.symbol ?? ""}",
             ),
             const SizedBox(
               height: 12,
             ),
-
             FittedBox(
               child: Row(
                 children: [
-                  Text(
-                    "${AppStrings.paymethod.tr}   :       ",
-                    style: AppStyles.semibold14(context)
-                        .copyWith(color: const Color(0xff8A8A8A)),
+                  SizedBox(
+                    width: 120,
+                    child: Text(
+                      "${AppStrings.paymethod.tr}     ",
+                      style: AppStyles.semibold14(context)
+                          .copyWith(color: const Color(0xff8A8A8A)),
+                    ),
                   ),
-                  IteamPayWay(
-                    title: offer.paymentMethod ?? '',
-                  ),
+                  IteamPayWay(title: p2pItem.paymentMethod ?? ""),
                 ],
               ),
             ),
             const SizedBox(
               height: 30,
-            ),
-            CustomButton(
-                onButtonPressed: () {
-                  Get.to(() => OfferDetails(
-                        offer: offer,
-                      ));
-                },
-                buttonText: AppStrings.viewdetails.tr),
-            // ignore: prefer_const_constructors
-            SizedBox(
-              height: 16,
             )
           ],
         ),
---
+++ lib/features/account/features/offer_details.dart/presentation/view/widget/user_info.dart
@@ -0,0 +1,73 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:horsely_app/core/services/translation/app_string.dart';
+import 'package:horsely_app/core/utils/app_text_styles.dart';
+import 'package:horsely_app/core/utils/image/app_images_svg.dart';
+import 'package:horsely_app/core/utils/image/custom_image_handler.dart';
+import 'package:horsely_app/features/account/features/offer/data/model/offers_model/datum.dart';
+import 'package:horsely_app/features/home/data/model/user_home_data/datum.dart';
+import 'package:horsely_app/routes/routes.dart';
+
+class Selinfo extends StatelessWidget {
+  const Selinfo({super.key, required this.item});
+  final OfferModel item;
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(16),
+          border: Border.all(color: Colors.black.withOpacity(.15), width: .5),
+          color: Colors.white,
+          boxShadow: [
+            BoxShadow(
+              color: Colors.black.withOpacity(.25),
+              blurRadius: 3,
+              spreadRadius: -1,
+            ),
+          ]),
+      child: ListTile(
+        leading: CircleAvatar(
+          radius: 25,
+          backgroundImage: NetworkImage(item.user?.image ?? ""),
+        ),
+        title: Row(
+          children: [
+            Text(
+              item.user?.name ?? "",
+              style: AppStyles.semibold16(context),
+            ),
+            const SizedBox(
+              width: 4,
+            ),
+            Text(
+              "(4.6)", //Todo :missed from api
+              style: AppStyles.semibold12(context)
+                  .copyWith(color: const Color(0xff8A8A8A)),
+            ),
+            const Icon(
+              Icons.star,
+              color: Colors.yellow,
+            )
+          ],
+        ),
+        subtitle: Text(
+          "${item.user?.orderCount} ${AppStrings.order.tr} |  ${item.user?.completedOrderCount} ${AppStrings.complete.tr}",
+          style: AppStyles.semibold12(context)
+              .copyWith(color: const Color(0xff8A8A8A)),
+        ),
+        trailing: GestureDetector(
+            onTap: () {
+              Get.toNamed(Routes.chat, arguments: {'userId': item.user?.id});
+            },
+            child: Container(
+              padding: const EdgeInsets.all(10),
+              width: 48,
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10),
+                  color: const Color(0xff199800).withOpacity(.15)),
+              child: const CustomImageHandler(AppImages.chatNotifications),
+            )),
+      ),
+    );
+  }
+}
